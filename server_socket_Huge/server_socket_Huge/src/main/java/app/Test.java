796163d1aca340ae93af43883805b344df350bc0
TruongChiNghiem1 <nghiem0859774418@gmail.com>
2024-05-23 10:16:47 +0700

    ds

--- server_socket_Huge/server_socket_Huge/src/main/java/app/Test.java
+++
@@ -1,9 +0,0 @@
-package app;
-
-import jakarta.persistence.Persistence;
-
-public class Test {
-	public static void main(String[] args) {
-		Persistence.createEntityManagerFactory("hospitaldatabase");
-	}
-}
